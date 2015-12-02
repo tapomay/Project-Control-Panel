31e3c1e74b1360f5133112e7de8ee7e6b3c754d3
Ketki Kulkarni <k.ketki22@gmail.com>
2015-12-02 14:24:24 -0800

    changes in graph files

--- app/js/graph.js
+++ app/js/graph.js
@@ -1,40 +1,24 @@
-
 // set up SVG for D3
 var width  = 960,
     height = 500,
     colors = d3.scale.category10();
 
-// var svg = d3.select('body')
-//   .append('svg')
-//   .attr('oncontextmenu', 'return false;')
-//   .attr('width', width)
-//   .attr('height', height);
-
 var svg = d3.select('svg');
+  
 
 // set up initial nodes and links
 //  - nodes are known by 'id', not by index in array.
 //  - reflexive edges are indicated on the node (as a bold black circle).
 //  - links are always source < target; edge directions are set by 'left' and 'right'.
-
-
 var nodes = [
-    // {id: 'job1', reflexive: false},
-    // {id: 'job2', reflexive: false },
-    // {id: 'job3', reflexive: false}
-  ];
-
-  /*var nodes[];
-  for(i in jobs)
-  {
-    var jobName = jobs[i].name;
-    alert(jobName);
-    nodes.push(jobName);
-  }*/
-  var lastNodeId = 0,
+    {id: 0, reflexive: false},
+    {id: 1, reflexive: true },
+    {id: 2, reflexive: false}
+  ],
+  lastNodeId = 2,
   links = [
-    // {source: nodes[0], target: nodes[1], left: false, right: true },
-    // {source: nodes[1], target: nodes[2], left: false, right: true }
+    {source: nodes[0], target: nodes[1], left: false, right: true },
+    {source: nodes[1], target: nodes[2], left: false, right: true }
   ];
 
 // init D3 force layout
@@ -42,7 +26,7 @@ var force = d3.layout.force()
     .nodes(nodes)
     .links(links)
     .size([width, height])
-    .linkDistance(200)    // distance of link or edge
+    .linkDistance(150)
     .charge(-500)
     .on('tick', tick)
 
@@ -50,9 +34,9 @@ var force = d3.layout.force()
 svg.append('svg:defs').append('svg:marker')
     .attr('id', 'end-arrow')
     .attr('viewBox', '0 -5 10 10')
-    .attr('refX', 12.5)           // set position of arrow along edge
-    .attr('markerWidth', 7)       //arrow height
-    .attr('markerHeight', 7)      //arrow width
+    .attr('refX', 6)
+    .attr('markerWidth', 3)
+    .attr('markerHeight', 3)
     .attr('orient', 'auto')
   .append('svg:path')
     .attr('d', 'M0,-5L10,0L0,5')
@@ -61,9 +45,9 @@ svg.append('svg:defs').append('svg:marker')
 svg.append('svg:defs').append('svg:marker')
     .attr('id', 'start-arrow')
     .attr('viewBox', '0 -5 10 10')
-    .attr('refX', 12.5)       // set position of arrow along edge
-    .attr('markerWidth', 7)   //arrow height
-    .attr('markerHeight', 7)  //arrow width
+    .attr('refX', 4)
+    .attr('markerWidth', 3)
+    .attr('markerHeight', 3)
     .attr('orient', 'auto')
   .append('svg:path')
     .attr('d', 'M10,-5L0,0L10,5')
@@ -94,7 +78,7 @@ function resetMouseVars() {
 // update force layout (called automatically each iteration)
 function tick() {
   // draw directed edges with proper padding from node centers
-    path.attr('d', function(d) {
+  path.attr('d', function(d) {
     var deltaX = d.target.x - d.source.x,
         deltaY = d.target.y - d.source.y,
         dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY),
@@ -151,21 +135,16 @@ function restart() {
   circle = circle.data(nodes, function(d) { return d.id; });
 
   // update existing nodes (reflexive & selected visual states)
-  //    on select event
   circle.selectAll('circle')
     .style('fill', function(d) { return (d === selected_node) ? d3.rgb(colors(d.id)).brighter().toString() : colors(d.id); })
     .classed('reflexive', function(d) { return d.reflexive; });
 
-  /*
-
-   add new nodes into graph
-
-   */
+  // add new nodes
   var g = circle.enter().append('svg:g');
 
   g.append('svg:circle')
     .attr('class', 'node')
-    .attr('r', 25)
+    .attr('r', 12)
     .style('fill', function(d) { return (d === selected_node) ? d3.rgb(colors(d.id)).brighter().toString() : colors(d.id); })
     .style('stroke', function(d) { return d3.rgb(colors(d.id)).darker().toString(); })
     .classed('reflexive', function(d) { return d.reflexive; })
@@ -263,7 +242,8 @@ function mousedown() {
 
   // because :active only works in WebKit?
   svg.classed('active', true);
-   if(d3.event.ctrlKey || mousedown_node || mousedown_link) return;
+
+  if(d3.event.ctrlKey || mousedown_node || mousedown_link) return;
 
   // insert new node at point
   var point = d3.mouse(this),
@@ -277,7 +257,7 @@ function mousedown() {
 
 function mousemove() {
   if(!mousedown_node) return;
-  
+
   // update drag line
   drag_line.attr('d', 'M' + mousedown_node.x + ',' + mousedown_node.y + 'L' + d3.mouse(this)[0] + ',' + d3.mouse(this)[1]);
 
@@ -287,7 +267,6 @@ function mousemove() {
 function mouseup() {
   if(mousedown_node) {
     // hide drag line
-     alert("mouseup");
     drag_line
       .classed('hidden', true)
       .style('marker-end', '');
